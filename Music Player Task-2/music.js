28babe3d630a44f8556f447e2d477e6f570c162f
Athri-s <aatthhrriiathri123@gmail.com>
2025-07-30 15:51:36 +0530

    Add files via upload

---
+++ Music Player Task-2/music.js
@@ -0,0 +1,26 @@
+let trackList = [
+  {
+    name: "Faded",
+    path: "/music/Alan Walker - Faded.mp3",
+    img: "./images/img1.jpg",
+    singer: "sound.com",
+  },
+  {
+    name: "Believer",
+    path: "./music/Imagine Dragons - Believer.mp3",
+    img: "./images/img2.jpg",
+    singer: "sound.com",
+  },
+  {
+    name: "Smile and Die",
+    path: "./music/Lady Gaga Bruno Mars - Die With A Smile Official Music Video.mp3",
+    img: "./images/img3.jpg",
+    singer: "sound.com",
+  },
+  {
+    name: "XXX Change",
+    path: "./music/xxx- change song.mp3",
+    img: "./images/img4.jpg",
+    singer: "sound.com",
+  },
+];
